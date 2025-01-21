ed8f7007fb0bcafff5e1f308aba46d3c362157cc
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-21 22:48:48 +0100

    Project implementation

---
+++ LocationManager/Managers/LocationManager.swift
@@ -0,0 +1,81 @@
+//
+//  LocationManager.swift
+//  SMOC
+//
+//  Created by Javier Calatrava on 16/1/25.
+//
+
+import Foundation
+import CoreLocation
+
+@globalActor
+actor GlobalManager {
+    static var shared = GlobalManager()
+}
+
+@GlobalManager
+class LocationManager: NSObject, ObservableObject  {
+    private var clLocationManager: CLLocationManager? = nil
+
+    @MainActor
+    @Published var permissionGranted: Bool = false
+    private var internalPermissionGranted: Bool = false {
+         didSet {
+            Task { [internalPermissionGranted] in
+                await MainActor.run {
+                    self.permissionGranted = internalPermissionGranted
+                }
+            }
+        }
+    }
+    
+    @MainActor
+    @Published var speed: Double = 0.0
+    private var internalSpeed: Double = 0.0 {
+         didSet {
+            Task { [internalSpeed] in
+                await MainActor.run {
+                    self.speed = internalSpeed
+                }
+            }
+        }
+    }
+    
+   // @MainActor
+    init(clLocationManager: CLLocationManager = CLLocationManager()) {
+        super.init()
+        self.clLocationManager = clLocationManager
+        clLocationManager.delegate = self
+    }
+    
+    func checkPermission() {
+        clLocationManager?.requestWhenInUseAuthorization()
+    }
+}
+
+extension LocationManager: @preconcurrency CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        let statuses: [CLAuthorizationStatus] = [.authorizedWhenInUse, .authorizedAlways]
+        if statuses.contains(status) {
+            internalPermissionGranted = true
+            Task {
+                internalStartUpdatingLocation()
+            }
+        } else if status == .notDetermined {
+            checkPermission()
+        } else {
+            internalPermissionGranted = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        internalSpeed = location.speed
+    }
+    
+    private func internalStartUpdatingLocation() {
+        guard CLLocationManager.locationServicesEnabled() else { return }
+        clLocationManager?.startUpdatingLocation()
+    }
+}
--- LocationManagerTests/LocationManagerTests.swift
+++ LocationManagerTests/LocationManagerTests.swift
@@ -5,13 +5,64 @@
 //  Created by Javier Calatrava on 21/1/25.
 //
 
+import CoreLocation
 import Testing
 @testable import LocationManager
 
-struct LocationManagerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct ManagersTestTests {
+    
+ 
+    @Test func testAthorizacionRequestDenied() async throws {
+        let locationManagerMock = LocationManagerMock()
+        locationManagerMock.clAuthorizationStatus = .denied
+        let sut = await LocationManager(clLocationManager: locationManagerMock)
+        await sut.checkPermission()
+        // Wait for the @Published speed property to update
+        try await Task.sleep(nanoseconds: 200_000_000)
+        await #expect(sut.permissionGranted == false)
     }
 
+
+    @Test func testAthorizacionRequestAuthorized() async throws {
+        let locationManagerMock = LocationManagerMock()
+        locationManagerMock.clAuthorizationStatus =  .authorizedWhenInUse
+        let sut = await LocationManager(clLocationManager: locationManagerMock)
+        await sut.checkPermission()
+        // Wait for the @Published speed property to update
+        try await Task.sleep(nanoseconds: 200_000_000)
+        await #expect(sut.permissionGranted == true)
+    }
+    
+
+    @Test func testStartUpdatingLocation() async throws {
+        let locationManagerMock = LocationManagerMock()
+        locationManagerMock.clAuthorizationStatus =  .authorizedWhenInUse
+        let sut = await LocationManager(clLocationManager: locationManagerMock)
+        await sut.checkPermission()
+        // Wait for the @Published speed property to update
+        try await Task.sleep(nanoseconds: 200_000_000)
+               
+        await #expect(sut.speed == 10.00)
+    }
+}
+
+class LocationManagerMock: CLLocationManager {
+    var clAuthorizationStatus: CLAuthorizationStatus = .notDetermined
+    
+    override func requestWhenInUseAuthorization() {
+        delegate?.locationManager!(self, didChangeAuthorization: clAuthorizationStatus)
+    }
+    
+    override func startUpdatingLocation() {
+        let sampleLocation = CLLocation(
+            coordinate: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+            altitude: 10.0,
+            horizontalAccuracy: 5.0,
+            verticalAccuracy: 5.0,
+            course: 90.0,
+            speed: 10.0,
+            timestamp: Date()
+        )
+        delegate?.locationManager!(self, didUpdateLocations: [sampleLocation])
+    }
 }
