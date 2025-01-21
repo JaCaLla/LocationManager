d78453a4b460c014fa47350193742ab3395f330b
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-21 22:38:09 +0100

    Sample iOS Blank project

---
+++ LocationManager/LocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManagerApp.swift
+//  LocationManager
+//
+//  Created by Javier Calatrava on 21/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocationManagerTests/LocationManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManagerTests.swift
+//  LocationManagerTests
+//
+//  Created by Javier Calatrava on 21/1/25.
+//
+
+import Testing
+@testable import LocationManager
+
+struct LocationManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
